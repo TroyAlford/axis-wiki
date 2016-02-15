36d3c466a9d19cd153786ba757d714ee520e659f
Troy Alford <troy@troyalford.com>
2016-02-14 22:36:58 -0800

    Adjusted header spacing

--- app/modules/TopNavigation.js
+++ app/modules/TopNavigation.js
@@ -8,13 +8,11 @@ export default class TopNavigation extends React.Component {
       <section className="hero is-info is-left is-bold cp-topnavigation">
         <div className="hero-header">
           <header className="header">
-            <div className="container">
-              <div className="header-left title">
-                <strong>Axis</strong> Wiki
-              </div>
-              <div className="header-menu">
-                {/*<span className="header-item"><Link to="/w/home">Home</Link></span>*/}
-              </div>
+            <div className="header-left title">
+              <strong>Axis</strong> Wiki
+            </div>
+            <div className="header-menu">
+              {/*<span className="header-item"><Link to="/w/home">Home</Link></span>*/}
             </div>
           </header>
         </div>
