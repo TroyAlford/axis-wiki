132dd720df416201c589e6451d39597f8cebdf8b
Troy Alford <troy.alford@mycase.com>
2016-03-07 16:30:04 -0800

    Simplified Slug class with ES6 syntax

--- server/modules/slug.js
+++ server/modules/slug.js
@@ -1,16 +1,14 @@
-import _      from 'lodash'
-
-var Slug = module.exports = {
-  normalize: function (value, retain_path) {
-    var parsed = (/([/]?[\w\d -_]{1,}[/])?(.*)/g).exec(_.toLower(value));
+export default class Slug {
+  static normalize(value, retain_path) {
+     var parsed = (/([/]?[\w\d -_]{1,}[/])?(.*)/g).exec(value.toLowerCase());
 
     var path = parsed[1] || '';
     var slug = parsed[2]
-      .replace(/[^\w\d/_]/g, '-')         // replace all non-valid characters with -'s
+      .replace(/[^\w\d/_]/g, '-')          // replace all non-valid characters with -'s
       .replace(/-{2,}/g, '-')              // replace all sequential -'s with a single -
       .replace(/(^[-]{1,})|(-$){1,}/g, '') // remove all leading or trailing -'s
     ;
 
-    return retain_path ? path + slug : slug;
+    return retain_path ? path + slug : slug;   
   }
 }
