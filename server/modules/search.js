1a5a2f99182978422c05758192b2287c62b28121
Troy Alford <troy.alford@mycase.com>
2016-03-10 17:33:26 -0800

    Fixed an issue with loading the article text for tag listings

--- server/modules/search.js
+++ server/modules/search.js
@@ -4,6 +4,7 @@ import cheerio        from 'cheerio'
 import express        from 'express'
 import fs             from 'fs'
 import path           from 'path'
+import URL            from 'url'
 import utils          from 'fs-utils'
 
 import Article        from './article'
@@ -18,9 +19,13 @@ search.get('/tagged/:tag', function(request, response) {
   var tag  = _.trim(_.toLower(request.params.tag || '')),
       link = Links.get(tag),
       json = {
-        html: link.exists ? Article.html(tag) : '',
+        html: link.exists ? Article.html(tag, URL.parse(request_url(request))) : '',
         links: Tags.articles_tagged(tag)
       };
 
   return response.status(200).send(json);
 });
+
+function request_url(request) {
+  return URL.parse(request.protocol + '://' + request.get('host') + request.originalUrl);
+}
