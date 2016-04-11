3f15e40db1ec5c583eecba34c5a70519ba7fe951
Troy Alford <troy.alford@mycase.com>
2016-04-10 17:30:56 -0700

    :boom: removing nuclear-js

--- source/application/ComponentBase.js
+++ source/application/ComponentBase.js
@@ -1,7 +1,4 @@
 import React                           from 'react'
-import { Reactor, Store, toImmutable } from 'nuclear-js'
-
-const reactor = new Reactor();
 
 function autoBind(object) {
   var proto = object.constructor.prototype;
