bda2d88c3e8a51b21ed1a6905d8db76e841b5df9
Troy Alford <troy@troyalford.com>
2016-05-07 20:29:41 -0700

    Added v1 of character sheet

--- source/sheet/Attribute.js
+++ source/sheet/Attribute.js
@@ -2,7 +2,7 @@ import Editable         from '../sheet/Editable'
 
 export default ({ attr = [], className = '' }) => 
   <div className={`attr cols-${attr.cols || 1} ${className}`}>
-    <Editable className="name" value={attr.name} />
+    <Editable className="name" value={attr.name.replace(/[_-]/g, ' ')} />
     <Editable className="value" value={attr.value} />
     <Editable className="theory" value={attr.theory} />
     <Editable classname="mastery" value={attr.mastery} />
