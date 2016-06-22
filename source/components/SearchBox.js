79f5f86089783b02307e35baef761726c7caf383
Troy Alford <troy@troyalford.com>
2016-06-21 23:17:51 -0700

    :bug:fix - debounced search to 500ms

--- source/components/SearchBox.js
+++ source/components/SearchBox.js
@@ -1,13 +1,37 @@
+import ComponentBase      from '../application/ComponentBase'
 import { connect }        from 'react-redux'
 import { searchRequest }  from '../redux/actions/search'
 
+class SearchBox extends ComponentBase {
+  constructor(props) {
+    super(props)
+    this.state = { term: props.term }
+    this.debounced_search = _.debounce(this.search, 500)
+  }
+
+  componentWillReceiveProps(props) {
+    this.state = { term: props.term }
+  }
+
+  search() {
+    this.props.dispatch(searchRequest(this.state.term))
+  }
+
+  render() {
+    return (
+      <div className={`search-box control has-icon ${this.props.className}`}>
+        <input type="text" placeholder="Search..." value={this.state.term}
+          onChange={event => {
+            this.setState({ term: event.target.value })
+            this.debounced_search()
+          }}
+        />
+        <i className="icon icon-search fa"></i>
+      </div>
+    )    
+  }
+}
+
 export default connect(
   state => state.search
-)(({ className, dispatch, term }) =>
-  <div className={`search-box control has-icon ${className}`}>
-    <input type="text" placeholder="Search..." value={term}
-      onChange={event => dispatch(searchRequest(event.target.value))}
-    />
-    <i className="icon icon-search fa"></i>
-  </div>
-)
+)(SearchBox)
