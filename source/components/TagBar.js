0b89f54f6e506df934f9cc9d4bce81dffaad40e9
Troy Alford <troy@troyalford.com>
2016-09-05 20:30:33 -0700

    Simplifying binding for Article to tags

--- source/components/TagBar.js
+++ source/components/TagBar.js
@@ -1,34 +1,85 @@
+import * as React from 'react'
 import TagsInput from 'react-tagsinput'
+import { difference, isEqual, orderBy } from 'lodash'
 
-let TagBar = ({
-  className = 'tag-bar',
-  inputProps = {
+export default class TagBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tags: this.clean(props.tags, props),
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      tags: this.clean(nextProps.tags, nextProps),
+    })
+  }
+
+  clean(list, props = this.props) {
+    let tags = difference(list, props.banned)
+    if (props.unique) tags = orderBy(tags)
+    return tags
+  }
+
+  render() {
+    const {
+      className, tags, readonly, unique,
+      inputSettings, tagSettings, onChange,
+    } = this.props
+
+    return (
+      <TagsInput
+        className={className} value={tags} readonly={readonly}
+        inputProps={inputSettings} tagProps={tagSettings}
+        renderInput={props => !readonly && <input type="text" {...props} />}
+        renderTag={props =>
+          <span key={props.key} {...props}>
+            <a className="icon icon-tag" href={`/page/${props.tag}`}>{props.tag}</a>
+            { !readonly && <a className={tagSettings.classNameRemove} onClick={() => props.onRemove(props.key)} /> }
+          </span>
+        }
+        onChange={updated => {
+          let clean = this.clean(updated)
+          if (!isEqual(clean, this.state.tags))
+            this.props.onChange(clean)
+        }}
+        onlyUnique={unique}
+      />
+    )
+  }
+}
+
+TagBar.propTypes = {
+  banned: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
+  className: React.PropTypes.string,
+  onChange: React.PropTypes.func.isRequired,
+  readonly: React.PropTypes.bool.isRequired,
+  tags: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
+  unique: React.PropTypes.bool.isRequired,
+
+  input: React.PropTypes.shape({
+    className: React.PropTypes.string,
+    placeholder: React.PropTypes.string,
+  }),
+  tagSettings: React.PropTypes.shape({
+    className: React.PropTypes.string,
+    classNameRemove: React.PropTypes.string,
+  }),
+}
+TagBar.defaultProps = {
+  banned: [],
+  className: 'tag-bar',
+  onChange: () => {},
+  readonly: false,
+  tags: [],
+  unique: true,
+
+  inputSettings: {
     className: 'tag-bar-input',
     placeholder: 'add tag'
   },
-  tagProps = {
+  tagSettings: {
     className: 'tag',
     classNameRemove: 'remove'
   },
-  readonly = false,
-  tags = [],
-  onChange = () => {},
-  onlyUnique = true
-}) =>
-  <TagsInput
-    className={className}
-    value={tags}
-    inputProps={inputProps} tagProps={tagProps}
-    readonly={readonly}
-    renderInput={props => !readonly && <input type="text" {...props} />}
-    renderTag={props =>
-      <span key={props.key} {...props}>
-        <a className="icon icon-tag" href={`/page/${props.tag}`}>{props.tag}</a>
-        { !readonly && <a className={tagProps.classNameRemove} onClick={() => props.onRemove(props.key)} /> }
-      </span>
-    }
-    onChange={onChange}
-    onlyUnique={onlyUnique}
-  />
-
-export default TagBar
+}
