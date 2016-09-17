08227de6ce47e7be78ea0541f94e0379b0e1b364
Troy Alford <troy@troyalford.com>
2016-09-17 13:32:10 -0700

    Added config for TinyMCE to understand <include> tags, and make them non-editable.

--- source/config/editor.js
+++ source/config/editor.js
@@ -2,6 +2,9 @@ export default {
   auto_focus: true,
   autosave_ask_before_unload: false,
   inline: true,
+  convert_fonts_to_spans: true,
+  custom_elements: 'include',
+  extended_valid_elements: 'include[class|from|sections]',
   fixed_toolbar_container: '.article.page > .tab-set',
   formats: {
     aligncenter: {
@@ -10,17 +13,19 @@ export default {
     },
     underline: { inline: 'u', exact: true }
   },
+  noneditable_noneditable_class: 'noedit',
   menubar: false,
   plugins:
-    'anchor advlist autosave code fullscreen hr image link lists paste table',
+    'anchor advlist autosave code fullscreen hr image link lists noneditable paste table',
   toolbar:
     'code | formatselect | bold italic underline ' +
     '| alignleft aligncenter alignright alignjustify ' +
     '| bullist numlist | hr link | image table | removeformat',
   valid_elements:
-    'a[href|target|class|style],img[src|class|style|height|width],' +
-    '@[class|colspan|rowspan|style],th,td,' +
-    '@[class|style],' +
+    'a[id|href|target|class|style],' +
+    'img[id|src|class|style|height|width],' +
+    '@[id|class|colspan|rowspan|style],th,td,' +
+    '@[id|class|style],' +
     '-h1,-h2,-h3,-h4,-h5,-h6,' +
     '-table,-tr,br,hr,-blockquote,' +
     '-div,-p,-ul,-ol,-li,-b/strong,-i/em,-u,-s/strike/del,-center,-sup,-sub'
