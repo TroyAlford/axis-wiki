0c4803becb91faf5e760ba2bf9ea085cfbc5d41b
Troy Alford <troy@troyalford.com>
2017-01-25 21:51:55 -0800

    Fixing build pipeline.

--- rollup.application.js
+++ rollup.application.js
@@ -8,29 +8,30 @@ import uglify      from 'rollup-plugin-uglify'
 const PRODUCTION = (process.env.NODE_ENV || 'development') === 'production'
 const ENVIRONMENT = JSON.stringify(PRODUCTION ? 'production' : 'development')
 
+const EXTERNALS = {
+  'react': 'React',
+  'react-dom': 'ReactDOM',
+}
+
 const plugins = [
   json(),
-  replace({
-    'process.env.NODE_ENV': ENVIRONMENT
-  }),
+  replace({ 'process.env.NODE_ENV': ENVIRONMENT }),
   babel({
     babelrc: false,
-    exclude: [
-      'node_modules/**',
-      '**/*.json',
-    ],
+    exclude: ['node_modules/**', '**/*.json'],
     plugins: ['transform-object-rest-spread', 'lodash'],
-    presets: ['es2015-rollup', 'stage-0', 'react', 'jest'],
+    presets: ['es2015-rollup', 'react', 'jest'],
+  }),
+  commonjs({
+    ignoreGlobal: false,
+    include: ['node_modules/**'],
   }),
   nodeResolve({
     browser: true,
     jsnext: true,
     main: true,
     preferBuiltins: false,
-  }),
-  commonjs({
-    ignoreGlobal: true,
-    include: ['node_modules/**'],
+    skip: Object.keys(EXTERNALS),
   }),
 ]
 
@@ -41,17 +42,13 @@ if (PRODUCTION) {
 export default {
   entry: 'source/application/Application.js',
   exports: 'none',
-  external: ['react', 'react-dom', 'react-router'],
-  globals: {
-    'react': 'React',
-    'react-dom': 'ReactDOM',
-    'react-router': 'ReactRouter',
-  },
+  external: Object.keys(EXTERNALS),
+  globals: EXTERNALS,
   plugins,
   targets: [{
     dest: 'build/js/application.js',
     format: 'iife',
-    sourceMap: !PRODUCTION,
+    sourceMap: false, //!PRODUCTION,
     sourceMapFile: 'build/js/application.js',
   }],
   treeshake: true,
