ebd2a2990a8a268caf0ca7a80ccaab5190722c52
Troy Alford <troy@troyalford.com>
2017-03-16 19:57:32 -0700

    Fixing jshint errors

--- server/modules/monitor.js
+++ server/modules/monitor.js
@@ -1,11 +1,8 @@
 import bodyParser from 'body-parser'
-import express    from 'express'
-import os         from 'os'
+import express from 'express'
+import os from 'os'
 
 import { toPercent, toSize, toTime } from '../../utility/format'
-import { zip }    from 'lodash'
-
-let cpuUsage = process.cpuUsage()
 
 export default express()
   .use(bodyParser.json())                         // Parses application/json
@@ -15,26 +12,27 @@ export default express()
   const memory = process.memoryUsage()
   const system = {
     total: os.totalmem(),
-    free: os.freemem(),
+    free:  os.freemem(),
   }
 
   return response.status(200).send({
     process: {
       memory: {
-        total: toSize(memory.rss),
+        total:    toSize(memory.rss),
         external: toSize(memory.external),
+
         heap: {
           total: toSize(memory.heapTotal),
-          used: toSize(memory.heapUsed),
+          used:  toSize(memory.heapUsed),
           usage: toPercent(memory.heapUsed / memory.heapTotal),
-        }
+        },
       },
       uptime: toTime(process.uptime()),
     },
     system: {
       memory: {
-        free: toSize(system.free),
-        used: toSize(system.total - system.free),
+        free:  toSize(system.free),
+        used:  toSize(system.total - system.free),
         total: toSize(system.total),
         usage: toPercent((system.total - system.free) / system.total),
       },
--- server/modules/search.js
+++ server/modules/search.js
@@ -1,36 +1,98 @@
 import { includes, keys, sortBy, uniq, values } from 'lodash'
 
-import $           from 'cheerio'
-import express     from 'express'
-import path        from 'path'
-import utils       from 'fs-utils'
+import $ from 'cheerio'
+import express from 'express'
+import path from 'path'
+import utils from 'fs-utils'
 
 import { folders } from '../../config/server'
-import grep        from '../helpers/grep'
-import Links       from '../services/Links'
+import grep from '../helpers/grep'
+import Links from '../services/Links'
+
+function getTitleMatches(searchTerm) {
+  const pattern = new RegExp(searchTerm, 'gi')
+  return keys(Links.links)
+  .filter((slug) => {
+    const title = Links.get(slug).title || ''
+    return slug.match(pattern) // Slug directly matches
+        || title.match(pattern)
+  })
+  .map((matchedSlug) => {
+    const slug = Links.resolve(matchedSlug)
+    return {
+      slug,
+      alias: matchedSlug,
+      file:  path.join(folders.articles, `${slug}.html`),
+      type:  'article:title',
+    }
+  })
+}
+
+async function getContentMatches(searchTerm) {
+  return grep(searchTerm, folders.articles, { ext: 'html' })
+  .then(list =>
+    list.map((hit) => {
+      const filename = hit.file.split('/').pop()
+      const slug = filename.split('.').reverse().pop()
+
+      return {
+        slug,
+        file:    hit.file,
+        results: hit.results.map((result) => {
+          const text = $(result.text).text()
+          if (!text.match(new RegExp(searchTerm, 'gi'))) {
+            return null
+          }
+
+          return { ...result, text }
+        }).filter(result => !!result), // Only truthy results
+        type: 'article:content',
+      }
+    }).filter(hit => hit.results.length)
+  )
+}
+
+const HTML_CODES = {
+  '&': '&amp;',
+  "'": '&apos;',
+  '"': '&quot;',
+  '<': '&lt;',
+  '>': '&gt;',
+}
+function encode(input) {
+  let output = input
+
+  Object.keys(HTML_CODES).forEach((key) => {
+    const regex = new RegExp(`[${key}]`, 'g')
+    output = output.replace(regex, HTML_CODES[key])
+  })
+
+  return output
+}
 
 export default express()
-.get('/:search_term', async (request, response) => {
-  let search_term = encode(request.params.search_term).toLowerCase();
+.get('/:searchTerm', async (request, response) => {
+  const searchTerm = encode(request.params.searchTerm).toLowerCase()
 
   const resultSets = [
-    ...getTitleMatches(search_term),
-    ...await getContentMatches(search_term),
+    ...getTitleMatches(searchTerm),
+    ...await getContentMatches(searchTerm),
   ]
 
-  let indexed = {}
-  resultSets.forEach(result => {
+  const indexed = {}
+  resultSets.forEach((result) => {
     const current = indexed[result.file] || {}
     indexed[result.file] = {
-      ...current, ...result,
+      ...current,
+      ...result,
       aliases: uniq([...(current.aliases || []), result.alias]),
-      type: uniq([...(current.type || []), result.type]),
+      type:    uniq([...(current.type || []), result.type]),
     }
   })
 
   const results = values(indexed)
   .filter(result => !!result)
-  .map(entry => {
+  .map((entry) => {
     const $html = $(utils.readFileSync(entry.file))
     const link = Links.get(entry.slug)
 
@@ -40,83 +102,22 @@ export default express()
       aliases: entry.aliases.filter(alias => !!alias),
       results: entry.results,
       title:   link.title,
-      type:    entry.type
+      type:    entry.type,
     }
   })
 
-  const sorted = sortBy(results, result => {
+  const sorted = sortBy(results, (result) => {
     let order = ''
-    if (result.title.toLowerCase() === search_term
-      || includes(result.aliases, search_term))
+    if (result.title.toLowerCase() === searchTerm
+      || includes(result.aliases, searchTerm)) {
       order = 0
-    else if (includes(result.type, 'article:title'))
+    } else if (includes(result.type, 'article:title')) {
       order = 1
-    else
+    } else {
       order = 2
+    }
 
     return `${order}-${result.title}`
   })
   response.status(200).send(JSON.stringify(sorted))
 })
-
-function getTitleMatches(search_term) {
-  const pattern = new RegExp(search_term, 'gi')
-  return keys(Links.links)
-    .filter(slug => {
-      const title = Links.get(slug).title || ''
-      return slug.match(pattern) // Slug directly matches
-          || title.match(pattern)
-    })
-    .map(matched_slug => {
-      const slug = Links.resolve(matched_slug)
-      return {
-        slug,
-        alias: matched_slug,
-        file: path.join(folders.articles, `${slug}.html`),
-        type: 'article:title',
-      }
-    })
-  ;
-}
-
-async function getContentMatches(search_term) {
-  return grep(search_term, folders.articles, { ext: 'html' })
-    .then(list =>
-      list.map(hit => {
-        const filename = hit.file.split('/').pop(),
-              slug = filename.split('.').reverse().pop()
-
-        return {
-          slug,
-          file: hit.file,
-          results: hit.results.map(result => {
-            const text = $(result.text).text();
-            if (!text.match(new RegExp(search_term, 'gi')))
-              return null;
-
-            return { ...result, text }
-          }).filter(result => !!result), // Only truthy results
-          type: 'article:content',
-        }
-      }).filter(hit => hit.results.length)
-    )
-  ;
-}
-
-const html_codes = {
-  '&': '&amp;',
-  "'": '&apos;',
-  '"': '&quot;',
-  '<': '&lt;',
-  '>': '&gt;'
-}
-function encode(input) {
-  let output = input
-
-  Object.keys(html_codes).forEach(key => {
-    const regex = new RegExp(`[${key}]`, 'g')
-    output = output.replace(regex, html_codes[key])
-  })
-
-  return output
-}
