cbc1eaf0ddcb855699089d2f1e3317e598fafb5e
Troy Alford <troy@troyalford.com>
2017-04-01 16:52:32 -0700

    Fixing eslint errors

--- source/components/Editable.test.js
+++ source/components/Editable.test.js
@@ -1,22 +1,23 @@
-jest.unmock('./Editable');
-
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import TestUtils from 'react-addons-test-utils'
 import Editable from './Editable'
 
-describe('Editable', () => {
-  let parent    = null,
-      component = null,
-      rendered  = null
+jest.unmock('./Editable')
 
-  const Simulate  = TestUtils.Simulate
+describe('Editable', () => {
+  let parent = null
+  let component = null
+  let rendered = null
+
+  const Simulate = TestUtils.Simulate
 
   function render(element) { ReactDOM.render(element, parent) }
 
   beforeEach(() => {
     parent = document.createElement('div')
     component = ReactDOM.render(<Editable />, parent)
+    // eslint-disable-next-line react/no-find-dom-node
     rendered = () => ReactDOM.findDOMNode(component)
   })
 
@@ -25,7 +26,7 @@ describe('Editable', () => {
     expect(rendered().classList.contains('editable')).toEqual(true)
     expect(rendered().classList.contains('test-class')).toEqual(true)
 
-    render(<Editable readonly={true} />)
+    render(<Editable readonly />)
     expect(rendered().classList.contains('readonly')).toEqual(true)
   })
 
@@ -33,22 +34,22 @@ describe('Editable', () => {
     const changing = jest.genMockFunction()
     const change = jest.genMockFunction()
 
-    render(<Editable value='first' editing={true} onChanging={changing} onChange={change} />)
-    Simulate.change(rendered().children[0], { target: { value: 'second' }})
+    render(<Editable value="first" editing onChanging={changing} onChange={change} />)
+    Simulate.change(rendered().children[0], { target: { value: 'second' } })
     expect(changing).toBeCalledWith('second', 'first')
     expect(change).toBeCalledWith('second', 'first')
 
-    Simulate.change(rendered().children[0], { target: { value: 'third' }})
+    Simulate.change(rendered().children[0], { target: { value: 'third' } })
     expect(changing).toBeCalledWith('third', 'second')
     expect(change).toBeCalledWith('third', 'second')
   })
 
   it('handles cancellation of changes properly', () => {
     const cancel = jest.genMockFunction().mockReturnValue(false)
-    const none   = jest.genMockFunction()
+    const none = jest.genMockFunction()
 
-    render(<Editable value='first' editing={true} onChanging={cancel} onChange={none} />)
-    Simulate.change(rendered().children[0], { target: { value: 'second' }})
+    render(<Editable value="first" editing onChanging={cancel} onChange={none} />)
+    Simulate.change(rendered().children[0], { target: { value: 'second' } })
     expect(cancel).toBeCalledWith('second', 'first')
     expect(none).not.toBeCalled()
   })
@@ -56,21 +57,22 @@ describe('Editable', () => {
   it('renders checkboxes properly', () => {
     function validateCheckbox() {
       expect(rendered().children.length).toEqual(1)
-      let checkbox = rendered().children[0]
+      const checkbox = rendered().children[0]
       expect(checkbox.tagName).toEqual('INPUT')
       expect(checkbox.type).toEqual('checkbox')
       return checkbox
     }
 
-    render(<Editable value={true} editing={true} />)
+    // eslint-disable-next-line react/jsx-boolean-value
+    render(<Editable value={true} editing />)
     expect(component.editorType).toEqual('boolean')
     validateCheckbox()
 
-    render(<Editable value='test' type='boolean' editing={true} />)
+    render(<Editable value="test" type="boolean" editing />)
     expect(component.editorType).toEqual('boolean')
-    let checkbox = validateCheckbox()
+    const checkbox = validateCheckbox()
 
-    Simulate.change(checkbox, { target: { value: false }})
+    Simulate.change(checkbox, { target: { value: false } })
     expect(component.dirty).toEqual(true)
     expect(rendered().classList.contains('dirty')).toEqual(true)
     expect(component.state.value).toEqual(false)
