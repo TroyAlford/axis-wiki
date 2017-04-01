de88b3c9b6f853b4ad00b52361a35f886fdbb4f9
Troy Alford <troy@troyalford.com>
2017-04-01 16:28:17 -0700

    Tags now correctly slugify and enforce uniqueness

--- source/components/TagBar.js
+++ source/components/TagBar.js
@@ -1,69 +1,66 @@
 import * as React from 'react'
 import TagsInput from 'react-tagsinput'
-import { difference, isEqual, orderBy } from 'lodash'
+import { isEqual } from 'lodash'
+import { slugify } from '../../utility/Slugs'
+
+const exclude = (list, exclusions) => list.filter(entry => exclusions.indexOf(entry) === -1)
+const clean = (list, bans) => slugify(exclude(slugify(list), slugify(bans)))
 
 export default class TagBar extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      tags: this.clean(props.tags, props),
+      tags: clean(props.tags, props.banned || []),
     }
   }
   componentWillReceiveProps(nextProps) {
     this.setState({
-      tags: this.clean(nextProps.tags, nextProps),
+      tags: clean(nextProps.tags, nextProps.banned || []),
     })
-  }
-
-  clean(list, props = this.props) {
-    let tags = difference(list, props.banned)
-    if (props.unique) tags = orderBy(tags)
-    return tags
   }
 
   render() {
     const {
-      className, tags, readonly, unique,
-      inputSettings, tagSettings, onChange,
+      className, tags, readonly, onChange,
+      inputSettings, tagSettings,
     } = this.props
 
     return (
-      <TagsInput
-        className={className} value={tags} readonly={readonly}
-        inputProps={inputSettings} tagProps={tagSettings}
-        renderInput={props => {
-          const { addTag, onChange, placeholder, value, ...other } = props
-          return !readonly
-          ? <input type="text" ref="input" {...other}
-              placeholder={placeholder} value={value}
-              onChange={onChange}
-            />
-          : <span ref="input"></span>
-        }}
+      <TagsInput onlyUnique readonly={readonly} value={tags}
+        className={className} inputProps={inputSettings} tagProps={tagSettings}
+        renderInput={props => (!readonly
+          ? <input type="text" ref="input" {...props} />
+          : <span ref="input" />
+        )}
         renderTag={props =>
           <span key={props.key} className={props.className}>
             <a className="icon icon-tag" href={`/page/${props.tag}`}>{props.tag}</a>
-            { !readonly && <a className={tagSettings.classNameRemove} onClick={() => props.onRemove(props.key)} /> }
+            { !readonly &&
+              <a className={tagSettings.classNameRemove}
+                onClick={(event) => {
+                  event.stopPropagation()
+                  event.preventDefault()
+                  return props.onRemove(props.key)
+                }}
+              /> }
           </span>
         }
-        onChange={updated => {
-          let clean = this.clean(updated)
-          if (!isEqual(clean, this.state.tags))
-            this.props.onChange(clean)
+        onChange={(updated) => {
+          const cleaned = clean(updated, this.props.banned)
+          if (!isEqual(cleaned, this.state.tags)) onChange(cleaned)
         }}
-        onlyUnique={unique}
       />
     )
   }
 }
 
 TagBar.propTypes = {
-  banned: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
+  banned:    React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
   className: React.PropTypes.string,
-  onChange: React.PropTypes.func.isRequired,
-  readonly: React.PropTypes.bool.isRequired,
-  tags: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
-  unique: React.PropTypes.bool.isRequired,
+  onChange:  React.PropTypes.func.isRequired,
+  readonly:  React.PropTypes.bool.isRequired,
+  tags:      React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
+  unique:    React.PropTypes.bool.isRequired,
 
   input: React.PropTypes.shape({
     className: React.PropTypes.string,
@@ -75,19 +72,18 @@ TagBar.propTypes = {
   }),
 }
 TagBar.defaultProps = {
-  banned: [],
+  banned:    [],
   className: 'tag-bar',
-  onChange: () => {},
-  readonly: false,
-  tags: [],
-  unique: true,
+  onChange:  () => {},
+  readonly:  false,
+  tags:      [],
 
   inputSettings: {
-    className: 'tag-bar-input',
-    placeholder: 'add tag'
+    className:   'tag-bar-input',
+    placeholder: 'add tag',
   },
   tagSettings: {
-    className: 'tag',
-    classNameRemove: 'remove'
+    className:       'tag',
+    classNameRemove: 'remove',
   },
 }
