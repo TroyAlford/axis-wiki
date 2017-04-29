66491f8a35d4b56260d4f83dbdae722b74903e39
Troy Alford <troy@troyalford.com>
2017-04-28 23:06:14 -0700

    Updating react-ace and stripping DOCTYPE warnings

--- source/components/HtmlEditor.js
+++ source/components/HtmlEditor.js
@@ -2,21 +2,42 @@ import * as React from 'react'
 import AceEditor from 'react-ace'
 
 import 'brace/mode/html'
+import 'brace/theme/chrome'
 import 'brace/ext/searchbox'
 
 export default class HtmlEditor extends React.Component {
   constructor(props) {
     super(props)
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.setEditor = this.setEditor.bind(this)
+
     this.options = {
       ...HtmlEditor.defaultProps.options,
       ...props.options,
     }
+  }
+  componentDidMount() {
+    this.editor.session.on('changeAnnotation', (_, session) => {
+      const count = session.$annotations.length
+      const updated = session.$annotations.filter(annotation =>
+        !/doctype/i.test(annotation.text)
+      )
+
+      if (updated.length !== count) session.setAnnotations(updated)
+    })
   }
   componentWillReceiveProps(nextProps) {
     this.options = {
       ...HtmlEditor.defaultProps.options,
       ...nextProps.options,
     }
+  }
+  componentWillUnmount() {
+    this.editor = null
+  }
+
+  setEditor(editor) {
+    if (editor) this.editor = editor.editor
   }
 
   render() {
@@ -35,6 +56,7 @@ export default class HtmlEditor extends React.Component {
         theme="chrome"
         value={this.props.html}
         width="100%" height="100%"
+        ref={this.setEditor}
       />
     )
   }
