1e4fd80f2d9c819959ee84998285c9e232d8ebbc
Troy Alford <troy@troyalford.com>
2017-05-27 22:01:54 -0700

    Updating logic for updating profiles.

--- server/modules/my.js
+++ server/modules/my.js
@@ -1,5 +1,7 @@
 import bodyParser from 'body-parser'
 import express from 'express'
+import Facebook     from '../middleware/Facebook'
+import NoAnonymous  from '../middleware/NoAnonymous'
 
 import Profile from '../services/Profile'
 
@@ -7,20 +9,31 @@ export default express()
   .use(bodyParser.json())                         // Parses application/json
   .use(bodyParser.urlencoded({ extended: true })) // Parses application/x-www-form-encoded
 
-.get('/profile', (request, response) => {
+.get('/profile', NoAnonymous, (request, response) => {
   const { session: { id } } = request
   response.status(200).send(Profile.load(id))
 })
 .post('/profile', (request, response) => {
-  const { id: postedId, name, email } = request.body
-
-  // if request.session.id isn't set, this is a new Profile
-  const id = request.session.id || postedId
+  const authenticated = Boolean(request.session && request.session.id)
+  const { name, email } = request.body
+  const id = authenticated ? request.session.id : request.body.id
   const profile = { id, name, email }
 
-  if (!Profile.save(profile.id, profile)) {
-    return response.status(500).send('Unable to save profile.')
+  if (!id) return response.status(500).send('Invalid profile id')
+
+  if (authenticated) {
+    if (Profile.save(id, profile))
+      return response.status(200).send(profile)
+    else
+      return response.status(500).send('Unable to save profile.')
   }
 
-  return response.status(200).send(Profile.load(profile.id))
+  // If we got here, the user is non-authenticated
+  if (Profile.exists(id)) // Don't let the user update an existing profile
+    return response.status(401).send('You must be logged in to update your profile.')
+
+  if (Profile.save(id, profile)) // Create new profile
+    return response.status(200).send({ ...profile, id })
+
+  return response.status(500).send('Unable to save profile.')
 })
