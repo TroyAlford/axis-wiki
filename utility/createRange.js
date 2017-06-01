56e9df187a3ab5f327c9e1cfc0f285dd246a6141
Troy Alford <troy@troyalford.com>
2017-05-31 18:42:41 -0700

    Extracting createRange function

---
+++ utility/createRange.js
@@ -0,0 +1,5 @@
+export default (start, end) => {
+  const step = (start <= end) ? 1 : -1
+  const length = (Math.abs(end - start) / Math.abs(step)) + 1
+  return Array.apply(null, Array(length)).map((_, i) => start + (i * step))
+}
---
+++ utility/createRange.test.js
@@ -0,0 +1,24 @@
+jest.unmock('./createRange')
+
+/* eslint-disable import/first */
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import createRange from './createRange'
+
+describe('createRange()', () => {
+  it('returns 1 value when low === high', () => {
+    const expected = [1]
+    const actual = createRange(1, 1)
+    expect(actual).toEqual(expected)
+  })
+  it('calculates increasing ranges correctly', () => {
+    const expected = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+    const actual = createRange(-5, 5)
+    expect(actual).toEqual(expected)
+  })
+  it('calculates decreasing ranges correctly', () => {
+    const expected = [5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5]
+    const actual = createRange(5, -5)
+    expect(actual).toEqual(expected)
+  })
+})
