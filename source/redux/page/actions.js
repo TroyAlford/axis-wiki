92359b1ddecb544e8012d00bc0df0fd8f7effb7e
Troy Alford <troy@troyalford.com>
2017-06-11 20:21:05 -0700

    Adding redux setup for resolution detect based on media queries

--- source/redux/page/actions.js
+++ source/redux/page/actions.js
@@ -10,3 +10,20 @@ export const TYPES = [ARTICLE, PICTURE, PROFILE, SEARCH]
 export function setPage(type, data) {
   return { type, data }
 }
+
+export const SMALL = 'small'
+export const MEDIUM = 'medium'
+export const LARGE = 'large'
+
+export const LAYOUT = 'layout'
+
+export function getLayout() {
+  if (!window.matchMedia) return LARGE
+  if (window.matchMedia('screen and (max-width:  320px)').matches) return SMALL
+  if (window.matchMedia('screen and (max-width: 1024px)').matches) return MEDIUM
+  if (window.matchMedia('screen and (min-width: 1025px)').matches) return LARGE
+  return LARGE
+}
+export function updateLayout() {
+  return { type: LAYOUT, layout: getLayout() }
+}
