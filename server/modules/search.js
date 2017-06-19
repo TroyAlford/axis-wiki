075fd57041c9ebb45af925690f7661b52f33556c
Troy Alford <troy@troyalford.com>
2017-06-18 20:50:20 -0700

    Replacing search with database version

--- server/modules/search.js
+++ server/modules/search.js
@@ -1,126 +1,112 @@
-import { sortBy, values } from 'lodash'
-
-import $ from 'cheerio'
 import express from 'express'
-import path from 'path'
-import utils from 'fs-utils'
+import $ from 'cheerio'
+import { startCase } from 'lodash'
+import { flow } from '../../utility/flow'
+import { slugify } from '../../utility/Slugs'
+import Article from '../db/schema/Article'
 
-import SETTINGS from '../../config/server'
-import grep from '../helpers/grep'
-import unique from '../../utility/unique'
-import Links from '../services/Links'
-
-function getTitleMatches(searchTerm) {
-  const pattern = new RegExp(searchTerm, 'gi')
-  return Object.keys(Links.links)
-  .filter(slug => (Links.get(slug) || {}).exists === true)
-  .filter((slug) => {
-    const title = Links.get(slug).title || ''
-    return slug.match(pattern) // Slug directly matches
-        || title.match(pattern)
-  })
-  .map((matchedSlug) => {
-    const slug = Links.resolve(matchedSlug)
-    return {
-      slug,
-      alias: matchedSlug,
-      file:  path.join(SETTINGS.folders.articles, `${slug}.html`),
-      type:  'article:title',
-    }
-  })
+function lemmatize(array) {
+  // Placeholder for later
+  return array
 }
 
-async function getContentMatches(searchTerm) {
-  return grep(searchTerm, SETTINGS.folders.articles, { ext: 'html' })
-  .then(list =>
-    list.map((hit) => {
-      const filename = hit.file.split('/').pop()
-      const slug = filename.split('.').reverse().pop()
-
-      return {
-        slug,
-        file:    hit.file,
-        results: hit.results.map((result) => {
-          const text = $(result.text).text()
-          if (!text.match(new RegExp(searchTerm, 'gi'))) {
-            return null
-          }
-
-          return { ...result, text }
-        }).filter(result => !!result), // Only truthy results
-        type: 'article:content',
-      }
-    }).filter(hit => hit.results.length)
-  )
+function reverseString(string) {
+  return string.split('').reverse().join('')
 }
 
-const HTML_CODES = {
-  '&': '&amp;',
-  "'": '&apos;',
-  '"': '&quot;',
-  '<': '&lt;',
-  '>': '&gt;',
-}
-function encode(input) {
-  let output = input
+function extractMatch(text, match) {
+  const { 0: matchText, index } = match
+  const length = matchText.length
 
-  Object.keys(HTML_CODES).forEach((key) => {
-    const regex = new RegExp(`[${key}]`, 'g')
-    output = output.replace(regex, HTML_CODES[key])
-  })
-
-  return output
+  return flow([
+    t => t.slice(index - 25, index + length + 25),
+    t => t.slice(t.search(/(\w|\s)/)),
+    reverseString,
+    t => t.slice(t.search(/(\w|\s)/)),
+    reverseString,
+    t => t.replace(matchText, `<span class="highlight">${matchText}</span>`),
+    t => `...${t}...`,
+  ], text)
 }
 
+/* eslint-disable no-console */
 export default express()
 .get('/:searchTerm', async (request, response) => {
-  const searchTerm = encode(request.params.searchTerm).toLowerCase()
+  const { searchTerm, skip: SKIP = 0, limit: LIMIT = 10 } = request.params
 
-  const resultSets = [
-    ...getTitleMatches(searchTerm),
-    ...await getContentMatches(searchTerm),
-  ]
+  const terms = lemmatize(searchTerm.split(/\s/).filter(Boolean))
+  console.log(` ~> Searching for: ${searchTerm} [${terms.length} variant(s)]`)
 
-  const indexed = resultSets.reduce((hash, result) => {
-    const current = hash[result.file] || {}
-    return {
-      ...hash,
-      [result.file]: {
-        ...current,
-        ...result,
-        aliases: unique([...(current.aliases || []), result.alias]),
-        type:    unique([...(current.type || []), result.type]),
-      },
-    }
-  }, {})
+  const slugs = slugify(terms)
+  const regexes = terms.map(term => new RegExp(`\\b${term}\\b`, 'ig'))
 
-  const results = values(indexed)
-  .filter(result => !!result)
-  .map((entry) => {
-    const $html = $(utils.readFileSync(entry.file))
-    const link = Links.get(entry.slug)
+  const query = {
+    $or: [
+      ...regexes.map(re => ({ title: { $regex: re } })),
+      { slug: { $in: slugs } },
+      { aliases: { $in: slugs } },
+      { tags: { $in: slugs } },
+      ...regexes.map(re => ({ html: { $regex: re } })),
+    ],
+  }
+  return Article.find(query).then((articles) => {
+    console.log(` ~~> ${articles.length} match(es) found`)
 
-    return {
-      file:    entry.slug,
-      image:   $html.find('img').first().attr('src'),
-      aliases: entry.aliases.filter(alias => !!alias),
-      results: entry.results,
-      title:   link.title,
-      type:    entry.type,
-    }
+    return Promise.all(articles.map(article =>
+      new Promise((resolve) => {
+        // eslint-disable-next-line no-param-reassign
+        if (!article.title) article.title = startCase(article.slug)
+
+        const match = {
+          byTitle: Boolean(regexes.map(re => re.test(article.title)).filter(Boolean).length),
+          bySlug:  slugs.includes(article.slug),
+          byAlias: Boolean(slugs.filter(slug => article.aliases.includes(slug)).length),
+          byTag:   Boolean(slugs.filter(slug => article.tags.includes(slug)).length),
+          byText:  regexes.map((re) => {
+            const found = article.html.match(re)
+            return found ? found.length : 0
+          }).reduce((sum, m) => sum + m, 0),
+        }
+
+        const sortKey = [
+          !(match.byTitle || match.bySlug || match.byAlias),
+          match.byText,
+          !match.byTag,
+        ]
+        console.log(article.slug, sortKey)
+
+        resolve({ article, match, sortKey })
+      })
+    ))
   })
+  .then(all => all.sort((a, b) => {
+    if (a.sortKey < b.sortKey) return -1
+    if (a.sortKey > b.sortKey) return 1
+    return 0
+  }))
+  .then(all => all.slice(SKIP, LIMIT))
+  .then(all => all.map(({ article }) => {
+    const text = $(article.html).text()
+    const hits = []
+    regexes.forEach((re) => {
+      let match = re.exec(text)
 
-  const sorted = sortBy(results, (result) => {
-    let order = ''
-    if (result.title.toLowerCase() === searchTerm || result.aliases.indexOf(searchTerm) !== -1) {
-      order = 0
-    } else if (result.type.indexOf('article:title') !== -1) {
-      order = 1
-    } else {
-      order = 2
-    }
-
-    return `${order}-${result.title}`
+      while (match) {
+        hits.push({ text: extractMatch(text, match) })
+        match = re.exec(text)
+      }
+    })
+    return { article, hits }
+  }))
+  .then(all => all.map(({ article, hits }) => ({
+    file:    article.slug,
+    image:   $(article.html).find('img').first().attr('src'),
+    aliases: article.aliases,
+    results: hits,
+    title:   article.title,
+  })))
+  .then((results) => {
+    response.status(200).send(JSON.stringify(results))
   })
-  response.status(200).send(JSON.stringify(sorted))
+  .catch(console.error)
 })
