70189257164fe0e2ffcd0ab15a53cc9697e720ed
Troy Alford <troy@troyalford.com>
2017-06-19 00:01:29 -0700

    Improving Icon styling, adding auto-cursor if onClick is set

--- source/components/Icon.js
+++ source/components/Icon.js
@@ -1,11 +1,30 @@
-export default ({
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Icon = ({
   className = '',
   name = '',
   size = 'default',
-  onClick = (event => {})
+  onClick,
 }) => (
   <span
     className={`icon is-${size} ${className}`}
     onClick={onClick}
-  ><i className={`icon-${name}`}></i></span>
+    style={{ cursor: onClick ? 'pointer' : 'inherit' }}
+  ><i className={`icon-${name}`} /></span>
 )
+
+Icon.defaultProps = {
+  className: undefined,
+  name:      'settings',
+  size:      'default',
+  onClick:   undefined,
+}
+Icon.propTypes = {
+  className: PropTypes.string,
+  name:      PropTypes.string,
+  size:      PropTypes.oneOf(['default', 'small', 'medium', 'large']),
+  onClick:   PropTypes.func,
+}
+
+export default Icon
