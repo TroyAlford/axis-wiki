12c58719ebcfb5eb07b1aac7e6f0cf39a133268c
Troy Alford <troy@troyalford.com>
2018-02-11 11:13:41 -0800

    Clean up dependencies and build scripts

--- webpack.config.js
+++ webpack.config.js
@@ -19,11 +19,6 @@ const ENVIRONMENT = process.env.NODE_ENV
 const PRODUCTION = ENVIRONMENT === 'production'
 const SOURCEMAP = process.env.SOURCEMAP
 
-const library = 'axis-wiki'
-const filename = PRODUCTION ? `${library}.min.js` : `${library}.js`
-
-const PLACEHOLDER = 'PLACEHOLDER'
-
 const uglify = new webpack.optimize.UglifyJsPlugin({ minimize: true })
 const ConfigPlugin = new webpack.DefinePlugin({
   'process.env': VARIABLES.reduce((o, key) => {
@@ -32,82 +27,55 @@ const ConfigPlugin = new webpack.DefinePlugin({
   }, { NODE_ENV: JSON.stringify(ENVIRONMENT) })
 })
 const HoistPlugin = new webpack.optimize.ModuleConcatenationPlugin()
-const serverSideModules = fs.readdirSync('node_modules')
-  .filter(x => ['.bin'].indexOf(x) === -1)
-  .reduce((mods, mod) => Object.assign(mods, { [mod]: `commonjs ${mod}` }))
 
 const bundle = {
-  entry: PLACEHOLDER,
   module: {
-    loaders: [{
-      test:    /\.js$/,
-      loader:  'babel-loader',
-      exclude: /node_modules/,
-    }],
     rules: [{
-      use: [{
-        loader: 'babel-loader',
-        options: {
-          plugins: [
-            'lodash',
-            'syntax-async-functions',
-            'transform-class-properties',
-            'transform-object-rest-spread',
-            'transform-regenerator',
-          ],
-          presets: ['env', 'react'],
-        },
-      }],
+      test: /\.(woff2?|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=100000',
+    }, {
+      test: /\.s?css$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    }, {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
     }],
   },
   output: {
-    filename: PLACEHOLDER,
-    library,
-    path:     PLACEHOLDER,
-    libraryTarget:  'umd',
+    library: 'axis-wiki',
+    libraryTarget: 'umd',
     umdNamedDefine: true,
   },
-  plugins: PRODUCTION
-    ? [ConfigPlugin, HoistPlugin, uglify]
-    : [ConfigPlugin, HoistPlugin],
+  plugins: [ConfigPlugin, HoistPlugin, PRODUCTION && uglify].filter(Boolean),
+  resolve: {
+    extensions: ['.css', '.js', '.scss']
+  }
 }
 
-module.exports = [
-  Object.assign({}, bundle, {
-    /* Main JS Bundle */
-    devtool: SOURCEMAP ? 'inline-source-map' : 'none',
-    entry: `${__dirname}/source/application/Application.js`,
-    externals: {
-      'React': 'React',
-      'react': 'React',
-      'ReactDOM': 'ReactDOM',
-      'react-dom': 'ReactDOM',
-      'react-router': 'ReactRouter',
-    },
-    output: Object.assign({}, bundle.output, {
-      filename: 'application.js',
-      path: `${__dirname}/build/js`,
-    }),
-  }),
-
-  Object.assign({}, bundle, {
-    /* Dependencies JS Bundle */
-    entry: `${__dirname}/vendor/dependencies.js`,
-    output: Object.assign({}, bundle.output, {
-      filename: 'dependencies.js',
-      path: `${__dirname}/build/js`,
-    }),
-  }),
-
-  // Object.assign({}, bundle, {
-  //   /* Server */
-  //   entry: `${__dirname}/server/server.js`,
-  //   externals: serverSideModules,
-  //   output: {
-  //     filename: 'server.js',
-  //     path: `${__dirname}/build/server`,
-  //   },
-  //   plugins: PRODUCTION ? [uglify] : [],
-  //   target: 'node',
-  // })
-]
+module.exports = [{
+  ...bundle,
+  /* Main JS Bundle */
+  devtool: SOURCEMAP ? 'inline-source-map' : 'none',
+  entry: `${__dirname}/source/application/Application.js`,
+  externals: {
+    'React': 'React',
+    'react': 'React',
+    'ReactDOM': 'ReactDOM',
+    'react-dom': 'ReactDOM',
+    'react-router': 'ReactRouter',
+  },
+  output: {
+    ...bundle.output,
+    filename: 'application.js',
+    path: `${__dirname}/build/js`,
+  },
+}, {
+  ...bundle,
+  entry: `${__dirname}/vendor/dependencies.js`,
+  output: {
+    ...bundle.output,
+    filename: 'dependencies.js',
+    path: `${__dirname}/build/js`,
+  },
+}]
