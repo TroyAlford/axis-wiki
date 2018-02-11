b039ebc5589caa7cd5ea9aba81900e1ae7d8f2b3
Troy Alford <troy@troyalford.com>
2018-02-11 11:17:12 -0800

    Remove normalize.css, whatwg-fetch, and simplify router

--- vendor/dependencies.js
+++ vendor/dependencies.js
@@ -1,8 +1,5 @@
-import 'whatwg-fetch'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-import * as ReactRouter from 'react-router'
 
 window.React = React
 window.ReactDOM = ReactDOM
-window.ReactRouter = ReactRouter
