cd9a90e66698a5bb925354b384b34d95339e7f27
Troy Alford <troy@troyalford.com>
2018-02-11 17:33:16 -0800

    Correct bug with binding MenuItem

--- models/MenuItem.js
+++ models/MenuItem.js
@@ -1,9 +1,9 @@
 import { types } from 'mobx-state-tree'
 
-const MenuItem = types.model('MenuItem', () => ({
+const MenuItem = types.model('MenuItem', {
   children: types.optional(types.array(types.late(() => MenuItem)), []),
   text: '',
   url: '',
-}))
+})
 
 export default MenuItem
