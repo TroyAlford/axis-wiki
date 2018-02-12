87b5fdb5b87c1a4efaea0f431f6a4e9cc9d062f1
Troy Alford <troy@troyalford.com>
2018-02-12 02:01:42 -0800

    Add ProfilePage to available Page Types

---
+++ models/ProfilePage.js
@@ -0,0 +1,31 @@
+import { flow, types } from 'mobx-state-tree'
+import { optionalArrayOfStrings } from './commonModels'
+import { GET } from './fetch'
+
+const DEFAULTS = {
+  id: '',
+  email: '',
+  favorites: [],
+  name: '',
+  privileges: [],
+  tags: [],
+  type: 'profile',
+}
+
+export default types.model('ProfilePage', {
+  ...DEFAULTS,
+  favorites: optionalArrayOfStrings,
+  privileges: optionalArrayOfStrings,
+  tags: optionalArrayOfStrings,
+  type: types.optional(types.literal('profile'), 'profile'),
+}).actions(self => ({
+  /* eslint-disable no-param-reassign */
+  load: flow(function* ({ id }) {
+    const response = yield GET(id ? `/api/profile/${id}` : '/api/my/profile')
+    if (response.status === 200) {
+      const profile = yield response.json()
+      Object.assign(self, profile)
+    }
+  }),
+  /* eslint-enable no-param-reassign */
+}))
