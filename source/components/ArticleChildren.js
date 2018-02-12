98512c92aefbc1ca5b8f48cb231bb10cf38252b5
Troy Alford <troy@troyalford.com>
2018-02-11 23:51:05 -0800

    Simplify and restyle ArticleChildren

--- source/components/ArticleChildren.js
+++ source/components/ArticleChildren.js
@@ -1,46 +1,21 @@
 import React from 'react'
-import { startCase } from 'lodash'
-import Icon from './Icon'
+import { Link } from 'react-router-dom'
+import Icon from '@components/Icon'
 
 const ArticleChildren = ({ articles = [], caption = 'Child Articles', iconName = 'tag', numberOfColumns = 4 }) => {
-  if (!articles || !Array.isArray(articles) || !articles.length) {
-    return <div className="tag-browser is-hidden" />
-  }
-
-  const childWidth = `${100 / numberOfColumns}%`
-  const childStyle = {
-    maxWidth: childWidth,
-    minWidth: childWidth,
-    width: childWidth,
-  }
+  const children = articles.toJSON()
+  if (!children.length) return <div className="tag-browser is-hidden" />
 
   const links = articles
-    .map((item) => {
-      if (!item) return null
-      if (typeof item === 'string') return { slug: item, title: startCase(item) }
-      if (!item.slug) return null
-      if (!item.title) return { slug: item.slug, title: startCase(item.slug) }
-      return item
-    })
-    .filter(Boolean)
-    .sort((a, b) => a.title.localeCompare(b.title))
-    .map(({ slug, title }) =>
-      <div key={slug} className="article-link" style={childStyle}><a href={`/page/${slug}`}>{title}</a></div>
-    )
-
-  const rows = Math.ceil(links.length / numberOfColumns)
-  const containerStyle = {
-    height: `${(rows * 1.5) + 1}rem`,
-  }
+    .sort((a, b) => a.displayName.localeCompare(b.displayName))
+    .map(({ slug, displayName }) => (
+      <Link key={slug} to={`/page/${slug}`}>{displayName}</Link>
+    ))
 
   return (
-    <div className="article-children message is-info">
-      <div className="message-header header">
-        <Icon name={iconName} /> {caption}
-      </div>
-      <div className="container message-body" style={containerStyle}>
-        {links}
-      </div>
+    <div className="article-children">
+      <header><Icon name={iconName} /> {caption}</header>
+      <div className="link-list">{links}</div>
     </div>
   )
 }
