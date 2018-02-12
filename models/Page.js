31d7b25463178beaef75a3166373d9b4467e52ba
Troy Alford <troy@troyalford.com>
2018-02-12 02:01:11 -0800

    Add Profile to valid Page types

--- models/Page.js
+++ models/Page.js
@@ -1,6 +1,7 @@
 import { types } from 'mobx-state-tree'
 import ArticlePage from './ArticlePage'
 import MediaPage from './MediaPage'
+import ProfilePage from './ProfilePage'
 import SearchPage from './SearchPage'
 
-export default types.union(ArticlePage, MediaPage, SearchPage)
+export default types.union(ArticlePage, MediaPage, ProfilePage, SearchPage)
