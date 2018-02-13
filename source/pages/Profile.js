487d077e3b9458e039ca6bb29fd5355976a69acb
Troy Alford <troy@troyalford.com>
2018-02-12 23:20:48 -0800

    Profile page rendering

--- source/pages/Profile.js
+++ source/pages/Profile.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react'
+import unique from '@utils/unique'
+import ArticleChildren from '@components/ArticleChildren'
 import ChildArticle from '@models/ChildArticle'
-import ArticleChildren from '../components/ArticleChildren'
+import Tag from '@components/Tag'
 
 @observer export default class Profile extends Component {
   render = () => {
-    const { favorites = [], id, name } = this.props.page
+    const {
+      articles = [],
+      favorites = [],
+      id,
+      name,
+      privileges = [],
+      tags,
+    } = this.props.page
+
+    const ownerOf = unique([...articles, ...tags]).sort()
 
     return (
       <div className="profile page">
-        <h1 className="name">{name}</h1>
-        <div className="picture">
-          <img alt="Profile Portrait" src={`//graph.facebook.com/${id}/picture?type=square&height=200`} />
+        <h1 className="user-name">{name}</h1>
+        <div className="profile">
+          <div className="user-picture">
+            <img alt="Profile Portrait" src={`//graph.facebook.com/${id}/picture?type=square&height=200`} />
+          </div>
+          <div className="privileges">
+            <div><b>Privileges</b></div>
+            {privileges.map(tag => <Tag key={tag} tag={tag} className="icon-settings" />)}
+          </div>
+          <div className="can-edit">
+            <div><b>Owner Of</b></div>
+            {ownerOf.map(tag => <Tag key={tag} tag={tag} className="icon-tag" />)}
+          </div>
         </div>
 
         <ArticleChildren
           articles={favorites.map(slug => ChildArticle.create({ slug }))}
           caption="Favorites"
+          className="favorites"
           iconName="favorite-on"
         />
       </div>
