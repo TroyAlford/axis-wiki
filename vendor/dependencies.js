a73bfe424f304d37e099273a685e086b50086775
Troy Alford <troy@troyalford.com>
2018-02-13 23:28:47 -0800

    Extract more dependencies into separate lib

--- vendor/dependencies.js
+++ vendor/dependencies.js
@@ -1,5 +1,13 @@
 import * as React from 'react'
+import MobX from 'mobx'
+import * as MobXReact from 'mobx-react'
+import * as MST from 'mobx-state-tree'
 import ReactDOM from 'react-dom'
+import * as ReactRouter from 'react-router-dom'
 
+window.MobX = MobX
+window.MobXReact = MobXReact
+window.MST = MST
 window.React = React
 window.ReactDOM = ReactDOM
+window.ReactRouter = ReactRouter
