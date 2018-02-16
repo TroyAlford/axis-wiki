168a2a44952904747440a243138ade12e9e0fbb5
Troy Alford <troy@troyalford.com>
2018-02-16 02:54:33 -0800

    First pass at laying out search results

--- models/SearchPage.js
+++ models/SearchPage.js
@@ -1,5 +1,6 @@
-import { types } from 'mobx-state-tree'
+import { flow, types } from 'mobx-state-tree'
 import { optionalArrayOfStrings } from './commonModels'
+import { GET } from './fetch'
 
 const MatchResult = types.model('MatchResult', {
   text: '',
@@ -8,15 +9,27 @@ const MatchResult = types.model('MatchResult', {
 const SearchResult = types.model('SearchResult', {
   aliases: optionalArrayOfStrings,
   file: '',
-  results: types.array(MatchResult),
+  image: '',
+  results: types.optional(types.array(MatchResult), []),
   title: '',
 })
 
 export default types.model('SearchPage', {
   term: '',
-  results: types.array(SearchResult),
+  results: types.optional(types.array(SearchResult), []),
   type: types.optional(types.literal('search'), 'search'),
 }).views(self => ({
   get title() { return `Search: ${self.term}` },
   get keywords() { return [self.term, self.type] },
+})).actions(self => ({
+  /* eslint-disable no-param-reassign */
+  load: flow(function* ({ term }) {
+    const response = yield GET(`/api/search/${term}`)
+    if (response.status !== 200) return
+
+    const results = yield response.json()
+    self.term = term
+    self.results = results
+  }),
+  /* eslint-enable no-param-reassign */
 }))
--- source/pages/Search.js
+++ source/pages/Search.js
@@ -1,92 +1,72 @@
 import React, { Component } from 'react'
+import { observer } from 'mobx-react'
+import { Link } from 'react-router-dom'
 import findIndex from 'lodash/findIndex'
 import includes from 'lodash/includes'
 
-export default class Search extends Component {
-  static defaultProps = {
-    loading: true,
-    results: [],
-    term: '',
-  }
-
-  getDisplayData = () => this.props.results.map((article) => {
-    const previews = (article.results || []).map((hit) => {
-      const words = hit.text.split(' ')
-      const index = findIndex(words, word => includes(
-        word.toLowerCase(),
-        this.props.term.toLowerCase()
-      ))
-
-      if (index < 0) { // not found
-        return { html: hit.text, line: hit.line }
-      }
-
-      return {
-        html: hit.text,
-        line: hit.line,
-      }
-    }).filter(preview => preview !== null)
-
-    return {
-      key: article.file,
-      hits: previews.length + article.aliases.length,
-      image: article.image,
-      slug: article.file,
-      title: article.title,
-      subtitle: article.aliases.join(', '),
-      previews,
-    }
-  })
-
-  handleResultClick = ({ target }) => {
-    window.routerHistory.push(`/page/${target.attributes.slug.value}`)
-  }
-  render() {
-    const results = this.getDisplayData()
-    if (this.props.loading) return <div className="search page loading" />
-    if (!results.length) return ( // eslint-disable-line curly
-      <div className="search page">
-        <div className="card no-results is-centered">
-          <div className="card-content">
-            <div className="content">No results found for this search term.</div>
-          </div>
-        </div>
-      </div>
-    )
-
+@observer export default class Search extends Component {
+  getDisplayData = () => {
+    const { results, term } = this.props.page
     return (
-      <div className="search page">{results.map(result => (
-        <div key={result.key} className="card result" slug={result.slug} onClick={this.handleResultClick}>
-          <div className="card-content">
-            <div className="media">
-              {result.image &&
-                <div className="media-left">
-                  <figure className="image is-64x64">
-                    <img src={result.image} alt={result.slug} disabled="disabled" />
-                  </figure>
-                </div>
-              }
-              <div className="media-content">
-                <nav className="level">
-                  <div className="level-left">
-                    <div className="level-item is-6"><b>{result.title}</b></div>
-                    <div className="level-item subtitle is-6"><i>{result.subtitle}</i></div>
-                  </div>
-                  <div className="level-right">
-                    <div className="level-item subtitle is-6"><i>{result.hits} hits</i></div>
-                  </div>
-                </nav>
-                {result.previews.slice(0, 3).map((preview, index) => (
-                  <div key={index} // eslint-disable-line react/no-array-index-key
-                    className="search-match"
-                    dangerouslySetInnerHTML={{ __html: preview.html }} // eslint-disable-line
-                  />
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
+      results.map((article) => {
+        const previews = (article.results || []).map((hit) => {
+          const words = hit.text.split(' ')
+          const index = findIndex(words, word => includes(
+            word.toLowerCase(),
+            term.toLowerCase()
+          ))
+
+          if (index < 0) { // not found
+            return { html: hit.text, line: hit.line }
+          }
+
+          return {
+            html: hit.text,
+            line: hit.line,
+          }
+        }).filter(preview => preview !== null)
+
+        return {
+          key: article.file,
+          hits: previews.length + article.aliases.length,
+          image: article.image,
+          slug: article.file,
+          title: article.title,
+          subtitle: article.aliases.join(', '),
+          previews,
+        }
+      })
+    )
+  }
+
+  renderResult = ({ key, slug, image, title, subtitle, hits, previews }) => (
+    <Link className="result card" key={key} to={`/page/${slug}`}>
+      {image && <div className="media" style={{ backgroundImage: `url(${image})` }} />}
+      <div className="title"><b>{title}</b> <i>{subtitle}</i></div>
+      <div className="hits"><i>{hits} hits</i></div>
+      <div className="results">
+        {previews.slice(0, 3).map((preview, index) => (
+          <div key={index} // eslint-disable-line react/no-array-index-key
+            className="search-match"
+            dangerouslySetInnerHTML={{ __html: preview.html }} // eslint-disable-line
+          />
+        ))}
+      </div>
+    </Link>
+  )
+  renderNoResults = () => (
+    <div className="card no-results is-centered">
+      <div className="content">No results found for this search term.</div>
+    </div>
+  )
+  render() {
+    if (this.props.page.loading) return <div className="search page loading" />
+
+    const results = this.getDisplayData()
+    return (
+      <div className="search page">
+        <h1>Search Results</h1>
+        {results.length ? results.map(this.renderResult) : this.renderNoResults()}
       </div>
     )
   }
