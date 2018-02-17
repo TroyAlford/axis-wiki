e210591ec168d9a3a29251ef84c516b4afbaa95d
Troy Alford <troy@troyalford.com>
2018-02-17 01:11:34 -0800

    Improve small-size search results

--- source/pages/Search.js
+++ source/pages/Search.js
@@ -39,21 +39,27 @@ import includes from 'lodash/includes'
     )
   }
 
-  renderResult = ({ key, slug, image, title, subtitle, hits, previews }) => (
-    <Link className="result card" key={key} to={`/page/${slug}`}>
-      {image && <div className="media" style={{ backgroundImage: `url(${image})` }} />}
-      <div className="results">
-        <div className="title"><b>{title}</b> <i>{subtitle}</i></div>
-        {previews.slice(0, 3).map((preview, index) => (
-          <div key={index} // eslint-disable-line react/no-array-index-key
-            className="search-match"
-            dangerouslySetInnerHTML={{ __html: preview.html }} // eslint-disable-line
-          />
-        ))}
-      </div>
-      <div className="hits"><i>{hits} hits</i></div>
-    </Link>
-  )
+  renderResult = ({ key, slug, image, title, subtitle, hits, previews }) => {
+    const classes = [
+      'result card',
+      image ? 'has-media' : 'no-media',
+    ].join(' ')
+    return (
+      <Link className={classes} key={key} to={`/page/${slug}`}>
+        {image && <div className="media" style={{ backgroundImage: `url(${image})` }} />}
+        <div className="results">
+          <div className="title"><b>{title}</b> <i>{subtitle}</i></div>
+          {previews.slice(0, 3).map((preview, index) => (
+            <div key={index} // eslint-disable-line react/no-array-index-key
+              className="search-match"
+              dangerouslySetInnerHTML={{ __html: preview.html }} // eslint-disable-line
+            />
+          ))}
+        </div>
+        <div className="hits"><i>{hits} hits</i></div>
+      </Link>
+    )
+  }
   renderNoResults = () => (
     <div className="card no-results is-centered">
       <div className="content">No results found for this search term.</div>
